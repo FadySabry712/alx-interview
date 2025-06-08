00ed13d16eb8e1205f10d938280d74a354bd067a
FadySabry712 <fadyasabry95@yahoo.com>
2025-06-08 14:21:20 +0000

    task 0

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-""" module for solving the 2D matrix """
+"""2D matrix rotation module solve alogortihm
+"""
 
 
 def rotate_2d_matrix(matrix):
-    """ function to set the algorithm """
-    l, r = 0, len(matrix) - 1
-
-    while l < r:
-        if i in range(r - l):
-            # move counter clockwise to set the new values
-            # decraisng the need of new temp pointers
-            top, bottom = l, r
-            top_left = matrix[top][l + i]
-            matrix[top][l + i] = matrix[bottom - i][l]
-            matrix[bottom - i][l] = matrix[bottom][r - i]
-            matrix[bottom][r - i] = matrix[top + i][r]
-            matrix[top + i][r] = top_left
+    """Rotates an m by n 2D matrix in place without using another matrix.
+    """
+    if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
+        return
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
         r -= 1
-        l += 1
