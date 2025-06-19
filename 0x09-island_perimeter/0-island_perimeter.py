8c3c2ee92a997b8eb913b5ea796932108616e664
FadySabry712 <fadysabry95@yahoo.com>
2025-06-19 12:13:38 -0700

    task0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python3
-""" Solution for Islan Perimeter problem"""
+""" Island Perimeter Solution """
 
 
 def island_perimeter(grid):
-    """ uses recursive function with a deep first search technqiue to get
-    the perimeter of the island
+    """ return perimeter of the island """
+    total_perimeter = 0
 
-    Retrun:
-    perimeter: (int) perimeter of the island
-    """
-    visit = set()
+    for i, row in enumerate(grid):
+        for j, element in enumerate(row):
+            # Check if element is land or sea
+            if (element == 0):
+                continue
 
-    def dfs(i, j):
-        """ recursive function ot get the perime
-        i: rows of the 2D grid
-        j: columns of the 2D grid
+            # Left check
+            if (j != 0 and row[j - 1] == 0):
+                total_perimeter += 1
+            if (j == 0):
+                # left case
+                total_perimeter += 1
 
-        Return:
-        value of the perim
-        """
-        if i >= len(grid) or j >= len(grid[0]) /
-        or i < 0 or j < 0 or grid[i][j] == 0:
-            return 1
-        if (i, j) in visit:
-            return 0
+            # Right check
+            if (j != len(row) - 1 and row[j + 1] == 0):
+                total_perimeter += 1
+            if (j == len(row) - 1):
+                # right case
+                total_perimeter += 1
 
-        visit.add((i, j))
-        perimeter = dfs(i, j + 1)
-        perimeter += dfs(i + 1, j)
-        perimeter += dfs(i, j - 1)
-        perimeter += dfs(i - 1, j)
-        return perimeter
+            # Upper check
+            if (i != 0 and grid[i - 1][j] == 0):
+                total_perimeter += 1
+            if (i == 0):
+                # top case
+                total_perimeter += 1
 
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j]:
-                return dfs(i, j)
+            # Bottom Check
+            if (i != len(grid) - 1 and grid[i + 1][j] == 0):
+                total_perimeter += 1
+            if (i == len(grid) - 1):
+                # bottom case
+                total_perimeter += 1
+
+    return total_perimeter
